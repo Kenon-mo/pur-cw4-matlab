459693b9ff7bf003e3a75d800139574662fd8b5b
Maciej Obara <maciejobara0@gmail.com>
2022-10-13 20:32:54 +0200

    Added FODT function

--- FODT.m
+++ FODT.m
@@ -6,20 +6,15 @@ function G_S = FODT(var_zeros, var_poles, k, tau, T_p)
 %   tau - input delay
 %   T_p - sampling period (0 for continuous objects)
 
-s = tf('s');
-
 if  size(var_poles) > 2
     T_h = sum(var_poles(3:end)) + sum(var_zeros);
 else
     T_h = 0;
 end
 
-
 T_hat = var_poles(1) + var_poles(2)/2;
 T_zhat = tau + var_poles(2)/2 + T_h + T_p/2;
 
-G_delay = tf(1,1,'inputDelay', T_zhat);
-G_S = k/(s*T_hat + 1) * G_delay;
+G_S = tf(k, [T_hat 1], 'inputDelay', T_zhat);
 
 end
-
